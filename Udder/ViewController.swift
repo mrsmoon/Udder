c0c9896531e6fdeac8e60c2a3872008ad8376525
sera <sera@seras-Mac.local>
2020-01-17 12:15:03 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by sera on 1/17/20.
+//  Copyright © 2020 sera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
